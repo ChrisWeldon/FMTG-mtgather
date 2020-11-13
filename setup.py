80a6285728c3a4a2ef21c2debf8f4f6224d3bc5f
Chris Evans <chrisevans@Chriss-MacBook-Air.local>
2020-11-13 12:12:21 -0500

    setup.py references requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup
 
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt")
+
+print(install_reqs)
+
 setup(name='mtgather',
       version='0.1',
       description='package for interfacing with MTG mysql server',
@@ -9,6 +19,7 @@ setup(name='mtgather',
       license='GNU',
       packages=['mtgather'],
       zip_safe=False,
+      install_requirements = install_reqs,
       test_suite='nose.collector',
       tests_require=['nose'],
 )
